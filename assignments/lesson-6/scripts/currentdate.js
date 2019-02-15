0fe9cd1ddfc93242297f771bb678c333c056bcec
brynewell <cbnewell@byui.edu>
2019-02-14 19:26:49 -0600

    commit

--- assignments/lesson-6/scripts/currentdate.js
+++ assignments/lesson-6/scripts/currentdate.js
@@ -1,64 +1,65 @@
-                
-        n =  new Date();
-        gd =  n.getDate();
-        y = n.getFullYear();
-        m = switch (n.getMonth()){
-            case 0:
-            day = "January";
-            break;
-            case 1:
-            day = "February";
-            break;
-            case 2:
-            day = "March";
-            break;
-            case 3:
-            day = "April";
-            break;
-            case 4:
-            day = "May";
-            break;
-            case 5:
-            day = "June";
-            break;
-            case 6:
-            day = "July";
-            break;
-            case 7:
-            day = "August";
-            break;
-            case 8:
-            day = "September";
-            break;
-            case 9:
-            day = "October"  
-            case 10:
-            day = "November"
-            break;
-            case 11:
-            day = "December"
-            break;
-        }
-            d = switch(n.getDay()) {
-            case 0:  
-            day = "Sunday";
-            break;
-            case 1:
-            day = "Monday";
-            break;
-            case 2:
-            day = "Tuesday";
-            break;
-            case 3:
-            day = "Wednesday";
-            break;
-            case 4:
-            day = "Thursday";
-            break;
-            case 5:
-            day = "Friday";
-            break;
-            case 6:
-            day = "Saturday";  
-            }
-        document.getElementById("date").innerHTML = d + " ," + gd + " " + m + " " + y
+n =  new Date();
+gd =  n.getDate();
+y = n.getFullYear();
+        var m;
+switch (n.getMonth()){
+    case 0:
+    m = "January";
+    break;
+    case 1:
+    m = "February";
+    break;
+    case 2:
+    m = "March";
+    break;
+    case 3:
+    m = "April";
+    break;
+    case 4:
+    m = "May";
+    break;
+    case 5:
+    m = "June";
+    break;
+    case 6:
+    m = "July";
+    break;
+    case 7:
+    m = "August";
+    break;
+    case 8:
+    m = "September";
+    break;
+    case 9:
+    m = "October";
+    break;
+    case 10:
+    m = "November";
+    break;
+    case 11:
+    m = "December";
+}
+                var day;
+    switch(n.getDay()) {
+    case 0:  
+    day = "Sunday";
+    break;
+    case 1:
+    day = "Monday";
+    break;
+    case 2:
+    day = "Tuesday";
+    break;
+    case 3:
+    day = "Wednesday";
+    break;
+    case 4:
+    day = "Thursday";
+    break;
+    case 5:
+    day = "Friday";
+    break;
+    case 6:
+    day = "Saturday";  
+    }
+document.getElementById("date").innerHTML = day + " ," + gd + " " + m + " " + y;

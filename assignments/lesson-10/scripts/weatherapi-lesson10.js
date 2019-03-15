51c8f45c6370275b5422efcc426b079850c36a02
brynewell <cbnewell@byui.edu>
2019-03-15 02:06:55 -0500

    commit

--- assignments/lesson-10/scripts/weatherapi-lesson10.js
+++ assignments/lesson-10/scripts/weatherapi-lesson10.js
@@ -12,5 +12,13 @@ weatherRequest.onload = function() {
     document.getElementById('lowtemp').innerHTML = weatherData.main.temp_min;
     document.getElementById('humidity').innerHTML = weatherData.main.humidity;
     document.getElementById('windspeed').innerHTML = weatherData.wind.speed;
-
 }
+let h = parseInt(document.getElementById('hightemp').innerHTML);
+let l = parseInt(document.getElementById('lowtemp').innerHTML);
+let t = (h + l)/2 ;
+let s = parseInt(document.getElementById('windspeed').innerHTML);
+
+let f = 35.74 + .06215*t - 35.75*(s**.16) + .4275*t*(s**.16);
+
+
+document.getElementById('windchill').innerHTML = f.toFixed(1) ;

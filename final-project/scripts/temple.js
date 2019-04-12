542f3d137bd1262de5e10f1ef2c87a98325a0aff
brynewell <cbnewell@byui.edu>
2019-04-12 17:01:55 -0500

    commit

---
+++ final-project/scripts/temple.js
@@ -0,0 +1,47 @@
+ var requestURL = 'https://brynewell.github.io/temple.json';
+ var request = new XMLHttpRequest();
+
+ request.open('GET', requestURL);
+
+ request.responseType = 'json';
+ request.send();
+
+ request.onload = function() {
+     var templeFill = request.response;
+     
+     document.getElementById('name1').innerHTML = temples[0].templeName;
+     document.getElementById('address1').innerHTML = temple[0].address;
+     document.getElementById('telephone1').innerHTML = temple[0].telephone;
+     document.getElementById('services1').innerHTML = temple[0].services;
+     document.getElementById('history1').innerHTML = temple[0].history;
+     document.getElementById('ordinance1').innerHTML = temple[0].ordinanceSchedule;
+     document.getElementById('session1').innerHTML = temple[0].sessionSchedule;
+     document.getElementById('closed1').innerHTML = temple[0].templeClosedDates;
+
+     document.getElementById('name2').innerHTML = temples[1].templeName;
+     document.getElementById('address2').innerHTML = temple[1].address;
+     document.getElementById('telephone2').innerHTML = temple[1].telephone;
+     document.getElementById('services2').innerHTML = temple[1].services;
+     document.getElementById('history2').innerHTML = temple[1].history;
+     document.getElementById('ordinance2').innerHTML = temple[1].ordinanceSchedule;
+     document.getElementById('session2').innerHTML = temple[1].sessionSchedule;
+     document.getElementById('closed2').innerHTML = temple[1].templeClosedDates;
+
+     document.getElementById('name3').innerHTML = temples[2].templeName;
+     document.getElementById('address3').innerHTML = temple[2].address;
+     document.getElementById('telephone3').innerHTML = temple[2].telephone;
+     document.getElementById('services3').innerHTML = temple[2].services;
+     document.getElementById('history3').innerHTML = temple[2].history;
+     document.getElementById('ordinance3').innerHTML = temple[2].ordinanceSchedule;
+     document.getElementById('session3').innerHTML = temple[2].sessionSchedule;
+     document.getElementById('closed3').innerHTML = temple[2].templeClosedDates;
+
+     document.getElementById('name4').innerHTML = temples[3].templeName;
+     document.getElementById('address4').innerHTML = temple[3].address;
+     document.getElementById('telephone4').innerHTML = temple[3].telephone;
+     document.getElementById('services4').innerHTML = temple[3].services;
+     document.getElementById('history4').innerHTML = temple[3].history;
+     document.getElementById('ordinance4').innerHTML = temple[3].ordinanceSchedule;
+     document.getElementById('session4').innerHTML = temple[3].sessionSchedule;
+     document.getElementById('closed4').innerHTML = temple[3].templeClosedDates;
+ }
